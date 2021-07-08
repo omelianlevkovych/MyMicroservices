cf6e7af74b9abd804ad49bc099261dba9c383e19
OmelianLevkovych <oml@e-boks.dk>
2021-07-08 18:26:40 +0300

    (discount-grpc) Initial project.

---
+++ src/Services/Discount/Discount.Grpc/Configuration/Interfaces/IConfigurationManager.cs
@@ -0,0 +1,9 @@
+﻿namespace Discount.Grpc.Configuration.Interfaces
+{
+    public interface IConfigurationManager
+    {
+        T GetConfiguration<T>(string key);
+
+        T GetConfigurationOrDefault<T>(string key, T defaultValue);
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Repositories/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.Grpc.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Repositories.Interfaces
+{
+    public interface IDiscountRepository
+    {
+        Task<CouponEntity> GetDiscount(string productName);
+        Task<bool> CreateDiscount(CouponEntity coupon);
+        Task<bool> UpdateDiscount(CouponEntity coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
